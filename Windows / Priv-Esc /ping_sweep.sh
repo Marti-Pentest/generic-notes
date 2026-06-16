20d168303a5a713e9e4feb1c13379fec45178a85
marti-sec <mhervas09@uoc.edu>
2026-06-16 09:45:43 +0200

    Update ping_sweep.sh

--- Windows / Priv-Esc /ping_sweep.sh
+++ Windows / Priv-Esc /ping_sweep.sh
@@ -1,7 +1,4 @@
-for i in $(seq 1 254); do
-    ping -c1 -W1 172.16.8.$i &>/dev/null && echo "172.16.8.$i is up" &
-done
-wait
+for i in $(seq 254); do ping 172.16.8.$i -c1 -W1 & done | grep from
 
 #Bash ping sweep script to discover live hosts in a /24 subnet.
 #Sends 1 ICMP packet with 1 second timeout to each host (1-254) in parallel using background processes.
